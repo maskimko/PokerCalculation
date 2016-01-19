461d0558b02e17be8a7111587bb2a56318bdf1b4
Maksym Shkolnyi <mshkolnyi@intropro.com>
2016-01-19 23:24:40 +0200

    Changed ordinal order of SuitSet. Ace was mover to the last position.

--- src/main/java/ua/pp/msk/poker/deck/SuitSet.java
+++ src/main/java/ua/pp/msk/poker/deck/SuitSet.java
@@ -11,10 +11,10 @@ package ua.pp.msk.poker.deck;
  */
 public enum SuitSet {
 
-    ACE((byte)0x1, "Ace", "A"), TWO((byte)0x2, "2", "2"), THREE((byte)0x3, "3", "3"), 
+     TWO((byte)0x2, "2", "2"), THREE((byte)0x3, "3", "3"), 
     FOUR((byte)0x4, "4", "4"), FIVE((byte)0x5, "5", "5"), SIX((byte)0x6, "6", "6"), 
     SEVEN((byte)0x7, "7", "7"), EIGHT((byte)0x8, "8", "8"), NINE((byte)0x9, "9", "9"), TEN((byte)0xA, "10", "10"), 
-    JACK((byte)0xB, "Jack", "J"), QUEEN((byte)0xD, "Queen", "Q"), KING((byte)0xE, "King", "K");
+    JACK((byte)0xB, "Jack", "J"), QUEEN((byte)0xD, "Queen", "Q"), KING((byte)0xE, "King", "K"), ACE((byte)0x1, "Ace", "A");
 
     private String name;
     private String letter;
