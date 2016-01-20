075b14f0f41954e253dbb5ec2fca1edf6c92811b
Maksym Shkolnyi <mshkolnyi@intropro.com>
2016-01-20 11:52:30 +0200

    Added per-stage counter

--- src/main/java/ua/pp/msk/poker/stat/Statistic.java
+++ src/main/java/ua/pp/msk/poker/stat/Statistic.java
@@ -19,6 +19,7 @@ public class Statistic {
     private static final Map<Combination, Integer> flopStatistic = new HashMap<>();
     private static final Map<Combination, Integer> turnStatistic = new HashMap<>();
     private static final Map<Combination, Integer> riverStatistic = new HashMap<>();
+    private static final Map<GameStage, Integer> stageOccurences = new HashMap<>();
     private static long counter = 0;
     private static GameStage stage = GameStage.preflop;
 
@@ -29,32 +30,32 @@ public class Statistic {
             turnStatistic.put(c, 0);
             riverStatistic.put(c, 0);
         }
+        for (GameStage gs : GameStage.values()) {
+            stageOccurences.put(gs, 0);
+        }
     }
-    
+
     public static synchronized void registerOccurance(Combination combination) {
         if (stage.equals(GameStage.preflop)) {
             int current = preFlopStatistic.get(combination);
-            current++;
-            preFlopStatistic.put(combination, current);
+            preFlopStatistic.put(combination, ++current);
         }
-         if (stage.equals(GameStage.flop)) {
+        if (stage.equals(GameStage.flop)) {
             int current = flopStatistic.get(combination);
-            current++;
-            flopStatistic.put(combination, current);
+            flopStatistic.put(combination, ++current);
         }
-          if (stage.equals(GameStage.turn)) {
+        if (stage.equals(GameStage.turn)) {
             int current = turnStatistic.get(combination);
-            current++;
-            turnStatistic.put(combination, current);
+            turnStatistic.put(combination, ++current);
         }
-           if (stage.equals(GameStage.river)) {
+        if (stage.equals(GameStage.river)) {
             int current = riverStatistic.get(combination);
-            current++;
-            riverStatistic.put(combination, current);
+            riverStatistic.put(combination, ++current);
         }
+        incrementStageOccurance(stage);
         counter++;
     }
-    
+
     public static Map<Combination, Integer> getPreFlopStatistic() {
         return preFlopStatistic;
     }
@@ -70,19 +71,25 @@ public class Statistic {
     public static Map<Combination, Integer> getRiverStatistic() {
         return riverStatistic;
     }
-    
+
     public static long getRegistrationsCount() {
         return counter;
     }
-    
-    public static void nextGameStage(){
+
+    public static void nextGameStage() {
         int ordinal = stage.ordinal();
         stage = GameStage.values()[++ordinal];
     }
+
     /**
      * It should be called every time a new game starts
      */
-    public static void resetStage(){
+    public static void resetStage() {
         stage = GameStage.preflop;
+    }
+
+    private static void incrementStageOccurance(GameStage gs) {
+        int times = stageOccurences.get(gs);
+        stageOccurences.put(gs, ++times);
     }
 }
